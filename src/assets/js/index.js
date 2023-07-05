f7a68fa3b40fb37a12f27f65a7ce0bb3b0549ee0
Jamie McArdle <mc4rdle@gmail.com>
2023-07-05 10:25:28 +0800

    updates

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -9,3 +9,18 @@ document.addEventListener("DOMContentLoaded", function () {
 	Glide();
 	LogoCarousel();
 });
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+	const elements = document.querySelectorAll('.heading-line');
+
+	elements.forEach(element => {
+	  if (element.classList.contains('headline-line')) {
+		console.log(element.textContent);
+		const firstLine = element.innerText.trim().split(' ');
+		const lastWord = firstLine[firstLine.length - 1];
+		console.log(`The last word on the first line of "${element.textContent}" is "${lastWord}".`);
+	  }
+	});
+  });
